009a4d2eb4e5658d1d8d7120b91b5f84ff9774c1
Leandro Scardua <leandroscar@gmail.com>
2017-07-15 10:21:20 -0400

    Add files via upload

---
+++ Copy_Group.ps1
@@ -0,0 +1,12 @@
+﻿Import-Module ActiveDirectory
+
+write-output "" 
+
+write-output "This script allows to copy a user's group and copy it to other user, the script collect information from the member of tab."
+
+write-output ""
+ 
+$srcUser = Read-Host "Please Enter the alias name of the source user " 
+$destUser = Read-Host "Please Enter the alias name of the Destination user " 
+
+foreach ($group in (Get-ADUser -Identity $srcUser -Properties MemberOf).MemberOf) {   Add-ADGroupMember -Identity $group -Members $destUser; }
